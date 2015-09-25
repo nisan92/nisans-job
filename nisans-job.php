04966acf1aa7eb89fb050608cf921c6f14232efa
Mustafa Kamal Hossain <nisan92@yahoo.com>
2015-09-25 22:32:10 +0600

    wordpress job portal
    Nisan-job Is a wordpress plugin that allow wordpress site to add a jib
    portal on his own site

---
+++ nisans-job.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Nisan's Job Portal managment 
+Plugin URI: http://mkhossain.info
+Description: A job posting and applying feature on wordpress site
+Version: 1.0.0
+Author: Mustafa Kamal Hossain Sabbir
+Author URI: http://www.mkhossain.info
+License: GPL2
+
+Copyright 2015 Mk softs(email: m@mkhossain.info    nisan92@yahoo.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA02110-1301USA
+
+*/
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This plugin is created by Md Mustafa kamal Hossain
+This plugin is for job managment only
